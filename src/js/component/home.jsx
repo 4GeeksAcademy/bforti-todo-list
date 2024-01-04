cbb7e19db064dc36bf676389be5bd927031914d3
Bforti <139017911+Bforti@users.noreply.github.com>
2024-01-04 23:40:20 +0000

    entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,70 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from 'react';
+
+
+
 
 //create your first component
 const Home = () => {
+	const [task, setTask ] = useState("");
+	const[lista, setLista]=   useState([]);
+
+	const guardar=(event) => {
+
+		if(event.key=="Enter"){
+			if(task.trim != ""){
+				setLista([...lista, task ])
+				setTask("")}
+			
+						    	}
+        
+ 							}
+	const eliminar=(event)=>{
+		let newarr=lista.filter((item, index)=>index!==event)
+		return setLista(newarr)
+		
+	}
+
+	const write=(e)=>(setTask(e.target.value))
+
+	
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="p-5">
+		<div className="form-floating d-flex justify-content-center">
+		  <input className="form-control"
+			 	type="text"
+				id="texto"
+			    onChange={write} 
+				value={task}
+				onKeyDown={guardar}/>
+		   <label for="texto">Lista</label>
+		 
 		</div>
-	);
+		<ul className="list-inline">
+			{lista.map((item, index)=>{
+				return(
+				     
+					   <li className="list-inline m-2 " key={index}>
+						 <div className="d-flex align-items-center">
+					   <button className="btn btn-sm btn-danger"
+					   onClick={()=>eliminar(index)}> X </button>
+					   <p className="mt-1 mx-2">{item}</p>
+					     </div>
+					   </li>
+					  )
+
+			})}
+
+
+		</ul>
+		</div>
+		
+		</>
+	)
+
+		
 };
 
 export default Home;
